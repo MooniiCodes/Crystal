aadfc586109e220def3456de14ed282fd01ab29a
sprite <153464041+ripooof@users.noreply.github.com>
2026-04-06 21:10:38 -0500

    make project compilable
    dude this took forever but it finally works, so im finally putting this on the repo

--- src/main.js
+++ src/main.js
@@ -8,9 +8,13 @@ import { xs } from './scenes/GameScene.js';
 
 const s = Phaser;
 
+// this bit code prevents the game from running on other websites besides geometrydash.com
+// it's probably here to prevent requiring the index-game.js on other domains, but the string encryption menthod sorta says otherwise
+/* 
 const Ts = window.location.hostname,
-    bs = [103, 101, 111, 109, 101, 116, 114, 121, 100, 97, 115, 104, 46, 99, 111, 109].map(_0x1c1bb4 => String.fromCharCode(_0x1c1bb4)).join('');
-// if (!(Ts === bs || Ts === "www." + bs || Ts.endsWith('.' + bs) || "localhost" === Ts)) throw document.body.innerHTML = '', new Error('');
+bs = [103, 101, 111, 109, 101, 116, 114, 121, 100, 97, 115, 104, 46, 99, 111, 109].map(_0x1c1bb4 => String.fromCharCode(_0x1c1bb4)).join('');
+if (!(Ts === bs || Ts === "www." + bs || Ts.endsWith('.' + bs) || "localhost" === Ts)) throw document.body.innerHTML = '', new Error('');
+*/
 const Ss = {
     'type': s.AUTO,
     'width': r,
