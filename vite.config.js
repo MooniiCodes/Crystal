b8e3272c2abee27e9e15ff3f743708dd7699426e
sprite <153464041+ripooof@users.noreply.github.com>
2026-04-10 00:23:26 -0500

    remove console logs when building for further accuracy

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,9 @@ export default defineConfig({
         minify: 'terser',
         terserOptions: {
             compress: {
-                drop_console: false, // for testing
+                pure_funcs: ['console.log', 'console.info'],
+                // drop_console: false, // false for testing
+                drop_debugger: true,
                 passes: 2
             },
             mangle: true,
